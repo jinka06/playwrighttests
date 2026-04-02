5e7e316c7801d73e7ec2473a57c6ecb268072625
jinka06 <jeremie.inkura@gmail.com>
2026-04-02 23:55:41 +0200

    feat: added file for the upload test

---
+++ tests/exerciseSetInputs.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import path from 'path';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://automationexercise.com/contact_us');
+  await page.getByRole('button', { name: 'Consent' }).click();
+});
+
+test('Upload a file', async ({ page }) => {
+  await page.goto('https://automationexercise.com/contact_us');
+
+  await page.getByPlaceholder('Name').fill('Jay');
+  await page.locator('input[name="email"]').fill('example@example.com');
+  await page.getByPlaceholder('Subject').fill('This is a test to automate an upload');
+  await page.locator('textarea').fill('I am just checking how to automate a file upload and button clicks');
+
+  const filePath = path.join(__dirname, '../fixtures/test.txt');
+  await page.locator('input[type="file"]').setInputFiles(filePath);
+  page.on('dialog', dialog => dialog.accept());
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.locator('#contact-page').getByText('Success! Your details have been submitted successfully.')).toBeVisible();
+});
